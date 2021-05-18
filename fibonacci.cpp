a642a4622953b3fbb4bf48ef9ba7549267635754
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-18 21:02:42 +0530

    Add files via upload
    fibonacci valuesfor first 20 numbers

---
+++ fibonacci.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+ 
+int a,b,c;
+ 
+cout<<"Enter the value of a";
+ 
+cin>>a;
+ 
+b=a+1;
+ 
+cout<<a<<"\n"<<b<<"\n";
+ 
+for(int i=b;i<=20;i++)
+ 
+{
+    
+    
+c=a+b;
+    
+a=b;
+    
+b=c;
+    
+cout<<c<<"\n";
+     
+     
+ 
+}
+
+    
+return 0;
+
+}
